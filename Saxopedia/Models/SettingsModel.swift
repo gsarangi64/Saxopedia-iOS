3fc85439c3cc3a54f0727f5d23c908b9228a6d46
gsarangi64 <gsarangi32768@gmail.com>
2025-12-03 03:10:32 -0500

    more settings

--- Saxopedia/Models/SettingsModel.swift
+++ Saxopedia/Models/SettingsModel.swift
@@ -6,16 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
 
 class SettingsModel: ObservableObject {
     @Published var darkMode: Bool {
-        didSet { UserDefaults.standard.set(darkMode, forKey: "darkMode") }
+        didSet {
+            UserDefaults.standard.set(darkMode, forKey: "darkMode")
+        }
     }
-
+    
     @Published var showComposerInfo: Bool {
-        didSet { UserDefaults.standard.set(showComposerInfo, forKey: "showComposerInfo") }
+        didSet {
+            UserDefaults.standard.set(showComposerInfo, forKey: "showComposerInfo")
+        }
     }
-
+    
     init() {
         self.darkMode = UserDefaults.standard.bool(forKey: "darkMode")
         self.showComposerInfo = UserDefaults.standard.bool(forKey: "showComposerInfo")
