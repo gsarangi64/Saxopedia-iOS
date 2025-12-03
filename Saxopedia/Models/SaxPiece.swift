436b0b1399868e0864b78784b06aed63104d5fdb
gsarangi64 <gsarangi32768@gmail.com>
2025-12-03 03:07:59 -0500

    hopefully successful encoding

--- Saxopedia/Models/SaxPiece.swift
+++ Saxopedia/Models/SaxPiece.swift
@@ -8,8 +8,7 @@
 import Foundation
 
 struct SaxPiece: Identifiable, Decodable {
-    var id: UUID = UUID()
-
+    let id = UUID()
     let title: String
     let composer: String
     let year: Int
@@ -19,8 +18,8 @@ struct SaxPiece: Identifiable, Decodable {
     let publisher: String
     let recording_url: String
     let notes: String
-
-    private enum CodingKeys: String, CodingKey {
+    
+    enum CodingKeys: String, CodingKey {
         case title, composer, year, instrumentation, difficulty, duration, publisher, recording_url, notes
     }
 }
