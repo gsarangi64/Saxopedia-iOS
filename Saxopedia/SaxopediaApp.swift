2729f29e522e0a7587fdbe39a2b6f0e2c2e8b955
gsarangi64 <gsarangi32768@gmail.com>
2025-12-03 02:56:24 -0500

    primary objects updated

--- Saxopedia/SaxopediaApp.swift
+++ Saxopedia/SaxopediaApp.swift
@@ -10,14 +10,17 @@ import SwiftUI
 @main
 struct SaxopediaApp: App {
     @StateObject private var repertoireService = SaxRepertoireService()
+    @StateObject private var composerService = ComposerAPIService()
     @StateObject private var settings = SettingsModel()
-
+    
     var body: some Scene {
         WindowGroup {
             NavigationStack {
                 HomeView()
                     .environmentObject(repertoireService)
+                    .environmentObject(composerService)
                     .environmentObject(settings)
+                    .preferredColorScheme(settings.darkMode ? .dark : .light)
             }
         }
     }
