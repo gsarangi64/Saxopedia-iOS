62f9d824f83c6085b314e9c55d6a5edb3bb51039
gsarangi64 <gsarangi32768@gmail.com>
2025-12-03 03:36:35 -0500

    was heavily overcomplicating settings model

--- Saxopedia/Models/SettingsModel.swift
+++ Saxopedia/Models/SettingsModel.swift
@@ -6,23 +6,8 @@
 //
 
 import Foundation
-import SwiftUI
 
 class SettingsModel: ObservableObject {
-    @Published var darkMode: Bool {
-        didSet {
-            UserDefaults.standard.set(darkMode, forKey: "darkMode")
-        }
-    }
-    
-    @Published var showComposerInfo: Bool {
-        didSet {
-            UserDefaults.standard.set(showComposerInfo, forKey: "showComposerInfo")
-        }
-    }
-    
-    init() {
-        self.darkMode = UserDefaults.standard.bool(forKey: "darkMode")
-        self.showComposerInfo = UserDefaults.standard.bool(forKey: "showComposerInfo")
-    }
+    @Published var darkMode = false
+    @Published var showComposerInfo = true
 }
