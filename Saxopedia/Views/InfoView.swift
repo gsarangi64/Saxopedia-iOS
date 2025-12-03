586da009fd03d567ed2a23b2c24bbd25438717eb
gsarangi64 <gsarangi32768@gmail.com>
2025-12-03 03:43:21 -0500

    succesful info view

--- Saxopedia/Views/InfoView.swift
+++ Saxopedia/Views/InfoView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct InfoView: View {
+    @EnvironmentObject var repertoireService: SaxRepertoireService
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section("App") {
+                HStack {
+                    Text("Name")
+                    Spacer()
+                    Text("Saxopedia")
+                        .foregroundColor(.secondary)
+                }
+                
+                HStack {
+                    Text("Version")
+                    Spacer()
+                    Text("1.0")
+                        .foregroundColor(.secondary)
+                }
+            }
+            
+            Section("Data") {
+                HStack {
+                    Text("Pieces Loaded")
+                    Spacer()
+                    Text("\(repertoireService.pieces.count)")
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .navigationTitle("About")
     }
 }
 
 #Preview {
-    InfoView()
+    NavigationStack {
+        InfoView()
+            .environmentObject(SaxRepertoireService())
+    }
 }
